6ec0f6bb59ec43f6a63d319fb8f7b4223d5c9899
Darren Croton <2062219+darrencroton@users.noreply.github.com>
2025-03-08 19:28:27 +1100

    Add ignore/ directory to .gitignore

--- ignore/temp/io_tree_binary.h
+++
@@ -1,9 +0,0 @@
-#ifndef IO_TREE_BINARY_H 
-#define IO_TREE_BINARY_H 
-
-// Proto-Types //
-
-void load_tree_table_binary(int32_t filenr); 
-void load_tree_binary(int32_t filenr, int32_t treenr); 
-void close_binary_file(void);
-#endif
